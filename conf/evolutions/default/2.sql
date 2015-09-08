2819224a3b81cea7971888d4b5c1bf22caf157e9
mrdShinse <shinse1128@hotmail.com>
2015-09-08 23:02:20 +0900

    create sample data

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -138,11 +138,31 @@ insert into user_roll (user_id, roll_id)
 insert into user_roll (user_id, roll_id)
   values (9, 9);
 
+insert into qanda_tag (qanda_id, tag_id)
+  values (1, 1);
+insert into qanda_tag (qanda_id, tag_id)
+  values (2, 2);
+insert into qanda_tag (qanda_id, tag_id)
+  values (3, 3);
+insert into qanda_tag (qanda_id, tag_id)
+  values (4, 4);
+insert into qanda_tag (qanda_id, tag_id)
+  values (5, 5);
+insert into qanda_tag (qanda_id, tag_id)
+  values (6, 6);
+insert into qanda_tag (qanda_id, tag_id)
+  values (7, 7);
+insert into qanda_tag (qanda_id, tag_id)
+  values (8, 8);
+insert into qanda_tag (qanda_id, tag_id)
+  values (9, 9);
+
 # --- !Downs
 
 delete from comment;
 delete from evaluation;
 delete from user_roll;
+delete from quanda_tag;
 delete from qanda;
 delete from roll;
 delete from tag;
