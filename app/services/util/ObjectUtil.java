c32bb3a938ef6fb4f1a3ff547abc44f360722e33
mrdShinse <shinse1128@hotmail.com>
2015-09-30 11:14:12 +0900

    create insert method

---
+++ app/services/util/ObjectUtil.java
@@ -0,0 +1,23 @@
+package services.util;
+
+import java.lang.reflect.Field;
+
+public class ObjectUtil {
+  /**
+   * 引数のインスタンスのフィールドを取得し、標準出力する。
+   */
+  public static void getFields(Object arg){
+    StringBuilder sb = new StringBuilder();
+    sb.append("Class: " + arg.getClass().getCanonicalName() + "\n");
+    sb.append("Settings:\n");
+    for (Field field : arg.getClass().getDeclaredFields()) {
+        try {
+            field.setAccessible(true);
+            sb.append(field.getName() + " = " + field.get(arg) + "\n");
+        } catch (IllegalAccessException e) {
+            sb.append(field.getName() + " = " + "access denied\n");
+        }
+    }
+    System.out.println(sb.toString());
+  }
+}
