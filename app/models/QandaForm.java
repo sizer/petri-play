fffe2aa006ae7a9c63ac63fa351c26d5d32868f3
mrdShinse <shinse1128@hotmail.com>
2015-10-24 23:16:49 +0900

    create answerList

--- app/models/QandaForm.java
+++ app/models/QandaForm.java
@@ -1,6 +1,7 @@
 package models;
 
 import play.data.validation.*;
+import utils.Boolean;
 
 public class QandaForm{
   @Constraints.Required
@@ -8,13 +9,19 @@ public class QandaForm{
   @Constraints.Required
   public String content;
   public String inputTags;
+  public boolean isQuestion;
 
   public QandaForm(){
 
   }
 
+  public QandaForm(boolean isQuestion){
+    this.isQuestion = isQuestion;
+  }
+
   public QandaForm(Qanda q){
     this.title = q.title;
     this.content = q.content;
+    this.isQuestion = Boolean.toBool(q.isQuestion);
   }
 }
